d6e4cbf35dfcb32a0703de5292c9a8a89f5747c2
holdeeno <48695629+holdeeno@users.noreply.github.com>
2023-05-19 23:02:38 +0000

    Additional testing

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from geopy.geocoders import Nominatim
 from shapely.geometry import shape, Point
 from fake_useragent import UserAgent, FakeUserAgentError
 import numpy as np
-# import folium
-# from folium.plugins import MarkerCluster
+import folium
+from folium.plugins import MarkerCluster
 
 def get_random_useragent():
   try:
@@ -57,23 +57,23 @@ def create_grid(city_polygon):
     print(f"An error occurred while creating the grid in the city: {e}")
     return []
 
-# def plot_grid_points(grid_points, filename):
-#     if grid_points:
-#         # Create a folium map centered at the first grid point
-#         folium_map = folium.Map(location=[grid_points[0][1], grid_points[0][0]], zoom_start=13)
+def plot_grid_points(grid_points, filename):
+    if grid_points:
+        # Create a folium map centered at the first grid point
+        folium_map = folium.Map(location=[grid_points[0][1], grid_points[0][0]], zoom_start=13)
 
-#         # Create a marker cluster
-#         marker_cluster = MarkerCluster().add_to(folium_map)
+        # Create a marker cluster
+        marker_cluster = MarkerCluster().add_to(folium_map)
 
-#         # Add all grid points to the map within the marker cluster
-#         for i, point in enumerate(grid_points):
-#             folium.CircleMarker([point[1], point[0]], radius=1, color="red").add_to(marker_cluster)
-#             if i % 100 == 0:  # log every 100 points
-#                 print(f"Plotted {i} points so far.")
+        # Add all grid points to the map within the marker cluster
+        for i, point in enumerate(grid_points):
+            folium.CircleMarker([point[1], point[0]], radius=1, color="red").add_to(marker_cluster)
+            if i % 100 == 0:  # log every 100 points
+                print(f"Plotted {i} points so far.")
 
-#         # Save the map to an HTML file
-#         folium_map.save(filename + '.html')
-#         print(f"Successfully plotted all {len(grid_points)} points.")
+        # Save the map to an HTML file
+        folium_map.save(filename + '.html')
+        print(f"Successfully plotted all {len(grid_points)} points.")
 
 def generate_google_maps_url(city_name, location_of_interest, point):
   # Order of coordinates is important for Google Maps
@@ -101,16 +101,24 @@ def generate():
     data = request.get_json()
     city_name = data['city_name']
     location_of_interest = data['location_of_interest']
-    urls_filename = f"urls_{city_name.replace(' ', '_')}_{location_of_interest.replace(' ', '_')}.txt"
-
-    city_polygon = get_city_polygon(city_name)
-    grid_points = create_grid(city_polygon)
-    urls = [generate_google_maps_url(city_name, location_of_interest, point) for point in grid_points]
-    write_urls_to_file(urls, urls_filename)
-
-    return jsonify({'message': 'URLs generated and saved to file successfully'}), 200
+    return process_generate(city_name, location_of_interest)
   except Exception as e:
     return jsonify({'error': 'Internal server error', 'message': str(e)}), 500
+
+def process_generate(city_name, location_of_interest):
+    try:
+        urls_filename = f"urls_{city_name.replace(' ', '_')}_{location_of_interest.replace(' ', '_')}.txt"
+        map_filename = f"map_{city_name.replace(' ', '_')}_{location_of_interest.replace(' ', '_')}"
+
+        city_polygon = get_city_polygon(city_name)
+        grid_points = create_grid(city_polygon)
+        plot_grid_points(grid_points, map_filename)
+        urls = [generate_google_maps_url(city_name, location_of_interest, point) for point in grid_points]
+        write_urls_to_file(urls, urls_filename)
+
+        return jsonify({'message': 'URLs generated and saved to file successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': 'Internal server error', 'message': str(e)}), 500
 
 @app.route('/generate_urls', methods=['GET'])
 def generate_urls():
@@ -121,10 +129,7 @@ def generate_urls():
     urls_filename = f"urls_{city_name.replace(' ', '_')}_{location_of_interest.replace(' ', '_')}.txt"
 
     if not os.path.exists(urls_filename):
-      # response = requests.post('http://localhost:5000/generate', json={'city_name': city_name, 'location_of_interest': location_of_interest})
-      response = requests.post('https://generate-google-maps-urls.holdeeno.repl.co/generate', json={'city_name': city_name, 'location_of_interest': location_of_interest})
-      if response.status_code != 200:
-        return jsonify({'error': 'Could not generate URLs', 'details': response.text}), 500
+      process_generate(city_name, location_of_interest)  # call the process_generate function directly
 
     with open(urls_filename, 'r') as f:
       urls = f.readlines()
@@ -132,6 +137,6 @@ def generate_urls():
 
   except Exception as e:
     return jsonify({'error': 'Internal server error', 'message': str(e)}), 500
-
+    
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
